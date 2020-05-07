8978a78e5ba273ed3038d5fbd704a76e384b2894
SoundaryaKorlapati <34760806+SoundaryaKorlapati@users.noreply.github.com>
2020-05-07 10:10:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! - testing");
-});
+// const server = http.createServer((request, response) => {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello World! - testing");
+// });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
+// const port = process.env.PORT || 1337;
+// server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+// console.log("Server running at http://localhost:%d", port);
 
 // const Joi = require('joi');
 // const express = require('express');
@@ -17,67 +17,67 @@ console.log("Server running at http://localhost:%d", port);
 // app.use(express.json());
 // const port = process.env.PORT || 3000;
 
-// app.listen(port, ()=>{
-//     console.log('Listening on port...')
-// })
+app.listen(port, ()=>{
+    console.log('Listening on port...')
+})
 
-// app.post('/hello', (req, res) => {
-//     res.send('hello world');
-// })
+app.post('/hello', (req, res) => {
+    res.send('hello world');
+})
 
-// app.post('/sendData', (req, res) => {
+app.post('/sendData', (req, res) => {
 
-//     const schema = {
-//         storeId: Joi.string().required(),
-//         upc: Joi.string().required(),
-//         epc: Joi.string().required(),
-//         ts: Joi.string().required(),
-//         tid: Joi.string().required(),
-//         userId: Joi.string().required(),
-//         printerId: Joi.string().required(),
-//         api_secret: Joi.string().required()
-//     };
+    const schema = {
+        storeId: Joi.string().required(),
+        upc: Joi.string().required(),
+        epc: Joi.string().required(),
+        ts: Joi.string().required(),
+        tid: Joi.string().required(),
+        userId: Joi.string().required(),
+        printerId: Joi.string().required(),
+        api_secret: Joi.string().required()
+    };
 
-//     const result = Joi.validate(req.body, schema);
+    const result = Joi.validate(req.body, schema);
     
-//     if(result.error){
-//         res.status(400).send(result.error.details[0].message);
-//         return;
-//     }
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
 
-//     const api_secret = req.body.api_secret
+    const api_secret = req.body.api_secret
 
-//     if(validSecret(api_secret)){
+    if(validSecret(api_secret)){
 
-//         const param = { 
-//             storeId : req.body.storeId, 
-//             upc : req.body.upc,
-//             epc : req.body.epc, 
-//             ts : req.body.ts, 
-//             tid : req.body.tid,
-//             userId : req.body.userId, 
-//             printerId : req.body.printerId 
-//         }
+        const param = { 
+            storeId : req.body.storeId, 
+            upc : req.body.upc,
+            epc : req.body.epc, 
+            ts : req.body.ts, 
+            tid : req.body.tid,
+            userId : req.body.userId, 
+            printerId : req.body.printerId 
+        }
     
-//         console.log(param);
-//         res.status(200).send();
+        console.log(param);
+        res.status(200).send();
 
-//     }
-//     else{
-//         res.status(400).send('Error : API Secret Invalid');
-//     }
+    }
+    else{
+        res.status(400).send('Error : API Secret Invalid');
+    }
     
 
-// })
+})
 
-// function validSecret(api_secret){
+function validSecret(api_secret){
 
-//     if(api_secret != ''){
+    if(api_secret != '123654896523sdf5sd6g561sg'){
 
-//         return false
+        return false
 
-//     }
+    }
 
-//     return true
+    return true
 
-// }
+}
