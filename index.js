2f9f67154b976ec5b621ae27f702d989cc4c8f22
SoundaryaKorlapati <34760806+SoundaryaKorlapati@users.noreply.github.com>
2020-05-07 10:36:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Joi = require('joi');
+//const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -13,59 +13,59 @@ app.get('/', (req, res) => {
     res.send('hello world');
 })
 
-app.post('/sendData', (req, res) => {
+// app.post('/sendData', (req, res) => {
 
-    const schema = {
-        storeId: Joi.string().required(),
-        upc: Joi.string().required(),
-        epc: Joi.string().required(),
-        ts: Joi.string().required(),
-        tid: Joi.string().required(),
-        userId: Joi.string().required(),
-        printerId: Joi.string().required(),
-        api_secret: Joi.string().required()
-    };
+//     const schema = {
+//         storeId: Joi.string().required(),
+//         upc: Joi.string().required(),
+//         epc: Joi.string().required(),
+//         ts: Joi.string().required(),
+//         tid: Joi.string().required(),
+//         userId: Joi.string().required(),
+//         printerId: Joi.string().required(),
+//         api_secret: Joi.string().required()
+//     };
 
-    const result = Joi.validate(req.body, schema);
+//     const result = Joi.validate(req.body, schema);
     
-    if(result.error){
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+//     if(result.error){
+//         res.status(400).send(result.error.details[0].message);
+//         return;
+//     }
 
-    const api_secret = req.body.api_secret
+//     const api_secret = req.body.api_secret
 
-    if(validSecret(api_secret)){
+//     if(validSecret(api_secret)){
 
-        const param = { 
-            storeId : req.body.storeId, 
-            upc : req.body.upc,
-            epc : req.body.epc, 
-            ts : req.body.ts, 
-            tid : req.body.tid,
-            userId : req.body.userId, 
-            printerId : req.body.printerId 
-        }
+//         const param = { 
+//             storeId : req.body.storeId, 
+//             upc : req.body.upc,
+//             epc : req.body.epc, 
+//             ts : req.body.ts, 
+//             tid : req.body.tid,
+//             userId : req.body.userId, 
+//             printerId : req.body.printerId 
+//         }
     
-        console.log(param);
-        res.status(200).send();
+//         console.log(param);
+//         res.status(200).send();
 
-    }
-    else{
-        res.status(400).send('Error : API Secret Invalid');
-    }
+//     }
+//     else{
+//         res.status(400).send('Error : API Secret Invalid');
+//     }
     
 
-})
+// })
 
-function validSecret(api_secret){
+// function validSecret(api_secret){
 
-    if(api_secret != 'b285d8e1d704afd077769dea5295685a'){
+//     if(api_secret != 'b285d8e1d704afd077769dea5295685a'){
 
-        return false
+//         return false
 
-    }
+//     }
 
-    return true
+//     return true
 
-}
+// }
